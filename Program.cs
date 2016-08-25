f448b47d832735be82734c314b64dd3fb167e199
Denis Korotin <deniskorotin4@gmail.com>
2016-08-25 15:19:46 +0300

    First lesson

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, S;
+            Console.WriteLine("Введите сторону а : ");
+            a = Convert.ToInt32(Console.ReadLine());           
+            
+            S = a * a;
+            Console.WriteLine("Площадь равна: "+S);
+            Console.ReadKey();
+        }
+    }
+}
